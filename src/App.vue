a2bdfd0ef87a9d43c276472ef638dc63c9a8e167
Hasin Hayder <countdraculla@gmail.com>
2024-01-08 22:01:18 +0600

    Accordion

--- src/App.vue
+++ src/App.vue
@@ -4,22 +4,22 @@ const accordions = ref([
   {
     heading: "What is term?",
     content: "Lorem ipsum dolor sit, amet consectetur adipisicing elit. Sequi dolor dolorum odio, saepe quibusdam iusto possimus nesciunt dolores assumenda quae totam, doloremque odit. Itaque cum animi, labore debitis deserunt iusto!",
-    isOpen:true,
+    isOpen: true,
   },
   {
     heading: "When to use Accordion Components?",
     content: "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Ut suscipit itaque, sequi incidunt ab sit optio debitis dignissimos doloribus ratione nobis id mollitia maiores eveniet necessitatibus dolorum praesentium! Corrupti, at.",
-    isOpen:false,
+    isOpen: false,
   },
   {
     heading: "How can it be defined?",
     content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Minima facilis labore voluptates provident nam, delectus suscipit nihil optio voluptate quae porro fugiat magni excepturi doloribus.",
-    isOpen:false,
+    isOpen: false,
   },
   {
     heading: "Chamber reached do he nothing be?",
     content: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ullam minima maiores magni dolore qui architecto pariatur incidunt repellat quos hic? Eius, eveniet accusantium! Quisquam, quo. Vitae ipsum ad veritatis commodi quasi expedita!",
-    isOpen:false,
+    isOpen: false,
   }
 ])
 
@@ -40,16 +40,16 @@ const accordions = ref([
           <div class="h-1 w-full mx-auto border-b my-5"></div>
 
           <!-- What is term -->
-          <div class="transition hover:bg-indigo-50" v-for="(accordion,index) in accordions" :key="index" >
+          <div class="transition hover:bg-indigo-50" v-for="(accordion, index) in accordions" :key="index">
             <!-- header -->
-            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16" >
-              <i class="fas fa-plus"></i>
-              <h3 >{{accordion.heading}}</h3>
+            <div class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16">
+              <i class="fas" :class="accordion.isOpen ? 'fa-minus' : 'fa-plus'"></i>
+              <h3 @click="accordion.isOpen = !accordion.isOpen">{{ accordion.heading }}</h3>
             </div>
             <!-- Content -->
-            <div class="px-5 pt-0 text-left pb-5" >
-              <p class="leading-6 font-light pl-9 ">
-              {{ accordion.content }}
+            <div class="px-5 pt-0 text-left pb-5">
+              <p class="leading-6 font-light pl-9" :class="accordion.isOpen ? '' : 'hidden'">
+                {{ accordion.content }}
               </p>
             </div>
           </div>
@@ -57,34 +57,8 @@ const accordions = ref([
       </div>
     </div>
 
-    <div class="w-full p-10 bg-gradient-to-br from-pink-50 to-indigo-100 grid place-items-center">
-      <div class="w-3/4 mx-auto rounded border">
-        <div class="bg-white p-10 shadow-sm">
-          <h3 class="text-lg font-medium text-gray-800">Several Windows stacked on each other</h3>
-          <p class="text-sm font-light text-gray-600 my-3">
-            The accordion is a graphical control element comprising a vertically stacked list of items such as labels or thumbnails
-          </p>
 
-          <div class="h-1 w-full mx-auto border-b my-5"></div>
 
-          <!-- What is term -->
-          <div class="transition hover:bg-indigo-50" v-for="(accordion,index) in accordions" :key="index" >
-            <!-- header -->
-            <div @click="accordion.isOpen=!accordion.isOpen"  class="accordion-header cursor-pointer transition flex space-x-5 px-5 items-center h-16" >
-              <i class="fas" :class="accordion.isOpen?'fa-minus':'fa-plus'"></i>
-              <h3>{{accordion.heading}}</h3>
-            </div>
-            <!-- Content -->
-            <div class="px-5 pt-0 text-left pb-5" v-show="true==accordion.isOpen" >
-              <p class="leading-6 font-light pl-9 ">
-              {{ accordion.content }}
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
-    
   </section>
 </template>
 
